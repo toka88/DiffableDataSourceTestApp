45fb65cd6cc2cc3af6867722a4f0e592c5fdbbaa
Goran Tokovic <tokan988@gmail.com>
2020-06-26 14:59:19 +0200

    Added tests

---
+++ DiffableDataSourceTestAppTests/Extensions/NumberFormatterTests.swift
@@ -0,0 +1,13 @@
+//
+//  NumberFormatterTests.swift
+//  DiffableDataSourceTestAppTests
+//
+//  Created by Goran Tokovic on 26/06/2020.
+//  Copyright © 2020 Goran Tokovic. All rights reserved.
+//
+
+import UIKit
+
+class NumberFormatterTests: XCTest {
+
+}
