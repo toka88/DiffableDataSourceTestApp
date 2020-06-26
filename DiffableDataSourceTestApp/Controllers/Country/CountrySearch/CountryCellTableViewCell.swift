9738640baa46e8ed1b3b4edaf72ce5d6d6b81e04
Goran Tokovic <tokan988@gmail.com>
2020-06-26 12:48:15 +0200

    Added error

--- DiffableDataSourceTestApp/Controllers/Country/CountrySearch/CountryCellTableViewCell.swift
+++ DiffableDataSourceTestApp/Controllers/Country/CountrySearch/CountryCellTableViewCell.swift
@@ -110,7 +110,7 @@ final class CountryCellTableViewCell: UITableViewCell {
 private extension UIImage {
     static func coloredImage(color: UIColor, size: CGSize = CGSize(width: 1, height: 1)) -> UIImage {
         let rect = CGRect(origin: .zero, size: size)
-        UIGraphicsBeginImageContextWithOptions(rect.size, false, 0.0)
+        UIGraphicsBeginIsmageContextWithOptions(rect.size, false, 0.0)
         color.setFill()
         UIRectFill(rect)
         let image = UIGraphicsGetImageFromCurrentImageContext()
